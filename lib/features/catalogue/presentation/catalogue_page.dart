435087348832c4be7408b4b450bf0f73a710e12f
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-03-25 10:26:49 +0530

    refactor: integrate Riverpod state management in CataloguePage

--- lib/features/catalogue/presentation/catalogue_page.dart
+++ lib/features/catalogue/presentation/catalogue_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:shopping_app/features/catalogue/application/product_notifier.dart';
 import 'package:shopping_app/features/catalogue/presentation/widgets/product_card.dart';
 
-class CataloguePage extends StatelessWidget {
+class CataloguePage extends ConsumerStatefulWidget {
   const CataloguePage({super.key});
 
   @override
+  ConsumerState<CataloguePage> createState() => _CataloguePageState();
+}
+
+class _CataloguePageState extends ConsumerState<CataloguePage> {
+  @override
   Widget build(BuildContext context) {
+    final products = ref.watch(productNotifierProvider);
+
     return Scaffold(
       backgroundColor: Color(0xFFFFEBEE),
       appBar: AppBar(
@@ -19,17 +28,21 @@ class CataloguePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.builder(
-        itemCount: 30,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.72,
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 6.0,
-        ),
-        itemBuilder: (context, index) {
-          return ProductCard();
-        },
+      body: products.when(
+        data:
+            (data) => GridView.builder(
+              itemCount: data.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.72,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 6.0,
+              ),
+              itemBuilder:
+                  (context, index) => ProductCard(product: data[index]),
+            ),
+        loading: () => Center(child: CircularProgressIndicator()),
+        error: (error, _) => Center(child: Text("Error: $error")),
       ),
     );
   }
