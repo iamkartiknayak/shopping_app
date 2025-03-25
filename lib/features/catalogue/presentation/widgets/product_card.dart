e93d9b04edddca7a8fe0df5974dc32b798103666
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-03-25 10:26:31 +0530

    refactor: update ProductCard to use fetched product data

--- lib/features/catalogue/presentation/widgets/product_card.dart
+++ lib/features/catalogue/presentation/widgets/product_card.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:shopping_app/features/catalogue/data/models/product_model.dart';
 import 'package:shopping_app/features/catalogue/presentation/widgets/add_to_cart_button.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({super.key});
+  const ProductCard({super.key, required this.product});
+
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +17,8 @@ class ProductCard extends StatelessWidget {
           Stack(
             children: [
               Image.network(
-                "https://cdn.dummyjson.com/products/images/groceries/Honey%20Jar/thumbnail.png",
-                fit: BoxFit.fill,
+                product.thumbnail,
+                fit: BoxFit.contain,
                 height: 180.0,
               ),
               AddToCartButton(onTap: () {}),
@@ -25,21 +28,21 @@ class ProductCard extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-
               children: [
                 Text(
-                  'Honey Jar',
+                  product.title,
+                  overflow: TextOverflow.ellipsis,
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
                 ),
                 SizedBox(height: 2.0),
-                Text('Groceries', style: TextStyle(fontSize: 12.0)),
+                Text(product.brand, style: TextStyle(fontSize: 12.0)),
                 SizedBox(height: 10.0),
                 RichText(
                   textAlign: TextAlign.start,
                   text: TextSpan(
                     children: [
                       TextSpan(
-                        text: '\$6.99 ',
+                        text: '\$${product.price} ',
                         style: TextStyle(
                           color: Colors.grey,
                           decoration: TextDecoration.lineThrough,
@@ -49,7 +52,7 @@ class ProductCard extends StatelessWidget {
                       ),
                       TextSpan(text: ' '),
                       TextSpan(
-                        text: '\$6.86',
+                        text: '\$${product.discountedPrice.toStringAsFixed(2)}',
                         style: TextStyle(
                           color: Colors.black,
                           fontSize: 14.0,
@@ -59,13 +62,14 @@ class ProductCard extends StatelessWidget {
                     ],
                   ),
                 ),
+                SizedBox(height: 2.0),
                 RichText(
                   textAlign: TextAlign.start,
                   text: TextSpan(
                     style: TextStyle(color: Colors.pink),
                     children: [
                       TextSpan(
-                        text: '1.91% ',
+                        text: '${product.discountPercentage}% ',
                         style: TextStyle(fontSize: 13.0),
                       ),
                       TextSpan(text: 'OFF'),
