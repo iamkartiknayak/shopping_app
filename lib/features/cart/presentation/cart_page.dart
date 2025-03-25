cf687c3804cf4489ecf58404b3372c1e331ee40c
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-03-26 00:02:07 +0530

    feat(cart): add navigator to access ProductPage

--- lib/features/cart/presentation/cart_page.dart
+++ lib/features/cart/presentation/cart_page.dart
@@ -7,6 +7,7 @@ import 'package:shopping_app/core/constants.dart';
 import 'package:shopping_app/features/cart/application/cart_notifier.dart';
 import 'package:shopping_app/features/cart/presentation/widgets/cart_summary_widget.dart';
 import 'package:shopping_app/features/cart/presentation/widgets/quantity_icon_button.dart';
+import 'package:shopping_app/features/product/presentation/product_page.dart';
 
 class CartPage extends ConsumerWidget {
   const CartPage({super.key});
@@ -61,84 +62,93 @@ class CartPage extends ConsumerWidget {
                 itemBuilder: (context, index) {
                   final product = cartItems.keys.elementAt(index);
                   final quantity = cartItems[product]!;
-                  return Container(
-                    color: Colors.white,
-                    margin: EdgeInsets.only(bottom: 8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(
-                          product.thumbnail,
-                          fit: BoxFit.contain,
-                          height: 160.0,
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 12.0),
-                              Text(product.title, style: kProductTitleStyle),
-                              SizedBox(height: 2.0),
-                              Text(
-                                product.brand,
-                                style: TextStyle(fontSize: 12.0),
-                              ),
-                              SizedBox(height: 8.0),
-                              ProductPriceTextSpan(
-                                originalPrice: product.price,
-                                discountedPrice: product.discountedPrice,
-                              ),
-                              SizedBox(height: 4.0),
-                              ProductDiscountTextSpan(
-                                discountPercentage: product.discountPercentage,
-                              ),
-                              SizedBox(height: 14.0),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Container(
-                                    height: 36.0,
-                                    width: 90.0,
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFFF5F5F5),
-                                      borderRadius: BorderRadius.circular(4),
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        QuantityIconButton(
-                                          icon: Icons.remove,
-                                          onTap:
-                                              () => cartNotifier.removeFromCart(
-                                                product,
-                                              ),
-                                        ),
-                                        Text(
-                                          '$quantity',
-                                          style: TextStyle(
-                                            color: kAccentColor,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        QuantityIconButton(
-                                          icon: Icons.add,
-                                          onTap:
-                                              () => cartNotifier.addToCart(
-                                                product,
-                                              ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(width: 30.0),
-                                ],
-                              ),
-                              SizedBox(height: 12.0),
-                            ],
+                  return InkWell(
+                    onTap:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductPage(product: product),
                           ),
                         ),
-                      ],
+                    child: Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            product.thumbnail,
+                            fit: BoxFit.contain,
+                            height: 160.0,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 12.0),
+                                Text(product.title, style: kProductTitleStyle),
+                                SizedBox(height: 2.0),
+                                Text(
+                                  product.brand,
+                                  style: TextStyle(fontSize: 12.0),
+                                ),
+                                SizedBox(height: 8.0),
+                                ProductPriceTextSpan(
+                                  originalPrice: product.price,
+                                  discountedPrice: product.discountedPrice,
+                                ),
+                                SizedBox(height: 4.0),
+                                ProductDiscountTextSpan(
+                                  discountPercentage:
+                                      product.discountPercentage,
+                                ),
+                                SizedBox(height: 14.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      height: 36.0,
+                                      width: 90.0,
+                                      decoration: BoxDecoration(
+                                        color: Color(0xFFF5F5F5),
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          QuantityIconButton(
+                                            icon: Icons.remove,
+                                            onTap:
+                                                () => cartNotifier
+                                                    .removeFromCart(product),
+                                          ),
+                                          Text(
+                                            '$quantity',
+                                            style: TextStyle(
+                                              color: kAccentColor,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          QuantityIconButton(
+                                            icon: Icons.add,
+                                            onTap:
+                                                () => cartNotifier.addToCart(
+                                                  product,
+                                                ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(width: 30.0),
+                                  ],
+                                ),
+                                SizedBox(height: 12.0),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
