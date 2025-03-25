09b77f12a76f4b1766e99967d30c07497e6e1701
Kartik Nayak <thisiskartiknayak@protonmail.com>
2025-03-25 11:58:49 +0530

    feat: implement  functionality for AddToCart button

--- lib/features/catalogue/presentation/widgets/product_card.dart
+++ lib/features/catalogue/presentation/widgets/product_card.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:shopping_app/common/product_discount_textspan.dart';
 import 'package:shopping_app/common/product_price_textspan.dart';
 import 'package:shopping_app/core/constants.dart';
+import 'package:shopping_app/features/cart/application/cart_notifier.dart';
 import 'package:shopping_app/features/catalogue/data/models/product_model.dart';
 import 'package:shopping_app/features/catalogue/presentation/widgets/add_to_cart_button.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends ConsumerWidget {
   const ProductCard({super.key, required this.product});
 
   final Product product;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       color: Colors.white,
       child: Column(
@@ -24,7 +26,14 @@ class ProductCard extends StatelessWidget {
                 fit: BoxFit.contain,
                 height: 180.0,
               ),
-              AddToCartButton(onTap: () {}),
+              AddToCartButton(
+                onTap: () {
+                  ref.read(cartProvider.notifier).addToCart(product);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("${product.title} added to cart!")),
+                  );
+                },
+              ),
             ],
           ),
           Padding(
